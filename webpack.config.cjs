84b9b12068fd6692c43c4c5176775fa7ed952061
leiboT <superviho1@gmail.com>
2023-07-12 03:20:49 +0800

    feat: corrected-package支持修正第三方依赖包的兼容性

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -67,6 +67,13 @@ const resolveClientEnv = (raw) => {
   }
 }
 
+// corrected-package 需修正的不支持目标浏览器的node_modules包
+const correctedPackage = require(resolveApp('package.json'))['corrected-package']
+let correctedPackageInclude = []
+if (correctedPackage && Array.isArray(correctedPackage)) {
+  correctedPackageInclude = correctedPackage.map((name) => resolveApp(`node_modules/${name}`))
+}
+
 module.exports = {
   entry: [resolveApp('src/main.tsx')],
   output: {
@@ -86,23 +93,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolveApp('src'), ...correctedPackageInclude]
+      },
+      {
         test: /\.tsx$/,
         use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    // NOTE usage 对于 node_modules 内的模块不起作用
-                    useBuiltIns: 'entry',
-                    corejs: '3.0'
-                  }
-                ]
-              ]
-            }
-          },
+          'babel-loader',
           {
             loader: 'ts-loader',
             options: {
